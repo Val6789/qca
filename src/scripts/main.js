8e18e2992b205594c2a73278165862fde8c9d191
Maximilien <pluchard.maximilien1234@gmail.com>
2019-01-15 15:54:20 +0100

    dot and electron display

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,22 @@
-/* global ThreeViewController:true */
+/* global ThreeViewController:true, DotDescriptor:true, THREE:true */
 /* exported orbit */
 
 var threeController = new ThreeViewController("viewport")
 threeController.startRenderLoop()
-threeController.addCube(1, 1, 1, 0.5)
+//threeController.addCube(1, 1, 1, 0.5)
 
-document.addEventListener("click", () => {
-  threeController.addCube(Math.random() * 4, Math.random() * 4, Math.random())
-})
+let d1 = new DotDescriptor(new THREE.Vector3(0, 0, 0), new THREE.Color(0.8, 0.3, 0.2))
+d1.addElectron(new THREE.Vector3(-1, 0, -1))
+d1.addElectron(new THREE.Vector3(1, 0, 1))
+threeController.addDot(d1)
+
+d1.position = new THREE.Vector3(2, 0, 0)
+threeController.addDot(d1)
+
+d1.position = new THREE.Vector3(4, 0, 0)
+threeController.addDot(d1)
+
+
+// document.addEventListener("click", () => {
+//   threeController.addCube(Math.random() * 4, Math.random() * 4, Math.random())
+// })
