c8c5a83723a1af7e3e2c8801022abc00e101381b
Maximilien <pluchard.maximilien1234@gmail.com>
2019-01-15 14:04:53 +0100

    skybox

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,7 @@
 
 var threeController = new ThreeViewController("viewport")
 threeController.startRenderLoop()
-threeController.addCube(0, 0, 1)
-threeController.addCube(0, 3, 2)
+threeController.addCube(1, 1, 1, 0.5)
 
 document.addEventListener("click", () => {
   threeController.addCube(Math.random() * 4, Math.random() * 4, Math.random())
--- src/scripts/treeViewController.js
+++ src/scripts/treeViewController.js
@@ -17,13 +17,82 @@ class ThreeViewController {
 
   addCube(x = 0, y = 0, z = 0, radius = 1) {
     let geometry = new THREE.BoxGeometry(radius, radius, radius)
-    let material = new THREE.MeshBasicMaterial({
+    let material = new THREE.MeshLambertMaterial({
       color: 0x00ff00
     })
     let mesh = new THREE.Mesh(geometry, material)
-    mesh.matrix.position = THREE.Vector3(x, y, z)
-    mesh.updateMatrix()
     this.scene.add(mesh)
+    mesh.position.set(x, y, z)
+  }
+
+  addSkybox() {
+    // const earthURL = "https://cdn-images-1.medium.com/max/800/1*UlLoXKAJcg7pqhjucMaksQ.png"
+    const path = "images/textures/skybox/"
+    const format = ".png"
+    const createUrl = (name) => path + name + format
+    const urls = [
+      createUrl("right"),
+      createUrl("left"),
+      createUrl("top"),
+      createUrl("bottom"),
+      createUrl("back"),
+      createUrl("front")
+    ]
+
+    let reflectionCubeTexture
+    this.waitTexture(urls)
+      .then((texture) => {
+        reflectionCubeTexture = texture
+        reflectionCubeTexture.format = THREE.RGBFormat
+
+        let shader = THREE.ShaderLib["cube"]
+        shader.uniforms["tCube"].value = reflectionCubeTexture
+
+        let material = new THREE.ShaderMaterial({
+          fragmentShader: shader.fragmentShader,
+          vertexShader: shader.vertexShader,
+          uniforms: shader.uniforms,
+          depthWrite: false,
+          side: THREE.BackSide
+        })
+
+        const s = 500
+        let mesh = new THREE.Mesh(new THREE.BoxGeometry(s, s, s), material)
+        this.scene.add(mesh)
+
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+
+  }
+
+
+  waitTexture(path) {
+    return new Promise((resolve, reject) => {
+      if (Array.isArray(path))
+        this.cubeLoader.load(
+          path,
+          (texture) => {
+            resolve(texture)
+          },
+          undefined,
+          (err) => {
+            console.error(err)
+            reject(err)
+          })
+      else
+        this.textureLoader.load(
+          path,
+          (texture) => {
+            resolve(texture)
+          },
+          undefined,
+          (err) => {
+            console.error(err)
+            reject(err)
+          })
+    })
   }
 
 
@@ -42,8 +111,8 @@ class ThreeViewController {
 
   // updates renderer parameters if the view changes.
   updateViewport() {
-    let width = this.renderer.domElement.clientWidth
-    let height = this.renderer.domElement.clientHeight
+    let width = this.parent.clientWidth
+    let height = this.parent.clientHeight
 
     this.camera.aspect = width / height
     this.camera.updateProjectionMatrix()
@@ -53,7 +122,7 @@ class ThreeViewController {
 
   constructor(canvasId) {
     // get viewport parent node
-    let parent = document.getElementById(canvasId)
+    this.parent = document.getElementById(canvasId)
 
     // boolean state enabling the render loop to be called every frame
     this.isRendering = false
@@ -69,11 +138,27 @@ class ThreeViewController {
     // initialize renderer
     this.renderer = new THREE.WebGLRenderer()
 
-    // use the device's pixel ratio (number of actual / physical screen pixels in one 'virtual' pixel: can be more than one on high res screens) 
-    this.renderer.setPixelRatio(window.devicePixelRatio)
+    // initialize texture Loader
+    this.textureLoader = new THREE.TextureLoader()
+      .setCrossOrigin(true)
+
+    this.cubeLoader = new THREE.CubeTextureLoader()
+      .setCrossOrigin(true)
+
+    // Add two lights
+    var ambiant = new THREE.AmbientLight(0xffffff)
+    this.scene.add(ambiant)
+
+    var point = new THREE.PointLight(0xffffff, 2)
+    this.scene.add(point)
+
+    this.addSkybox()
+
+    // use the device's pixel ratio (number of actual / physical screen pixels in one 'virtual' pixel: can be more than one on high res screens)
+    this.renderer.setPixelRatio(window.devicePixelRatio || 1)
 
     // inserts the WebGl canvas in the document
-    parent.appendChild(this.renderer.domElement)
+    this.parent.appendChild(this.renderer.domElement)
     this.updateViewport()
 
     // listen for viewport size changes
