b846f5011040cd4bf56e6a2f85303631fad168de
Maximilien <pluchard.maximilien1234@gmail.com>
2019-01-15 12:56:39 +0100

    clean

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,11 @@
+/* global ThreeViewController:true */
+/* exported orbit */
+
 var threeController = new ThreeViewController("viewport")
 threeController.startRenderLoop()
-threeController.addCube(0,0,1)
+threeController.addCube(0, 0, 1)
+threeController.addCube(0, 3, 2)
 
-document.addEventListener("click", ev => {
-    threeController.addCube(Math.random()*4,Math.random()*4,Math.random())
+document.addEventListener("click", () => {
+  threeController.addCube(Math.random() * 4, Math.random() * 4, Math.random())
 })
