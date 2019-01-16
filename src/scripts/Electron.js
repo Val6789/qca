9e5bca901ae36a2c2102786344811bb260a8c3f1
Pierre Gabory <piergabory@gmail.com>
2019-01-16 23:05:16 +0100

    Refactoring Dot and electron

--- src/scripts/Electron.js
+++ src/scripts/Electron.js
@@ -6,6 +6,41 @@ class Electron {
         return this.dot.position
     }
 
+    constructor(dot) {
+        //properties 
+        this.dot = dot
+        this.charge = 1.0
+
+        // insert new particle in the geometry attribute buffer
+        let attributeBuffer = Electron._particuleGeometryBuffer.getAttribute("position")
+        attributeBuffer.set(this.position.toArray(), Electron.instances.length * 3)
+        attributeBuffer.needsUpdate = true
+
+        // update buffer size
+        Electron._particuleGeometryBuffer.setDrawRange(0, Electron.instances.length + 1)
+        Electron._particuleGeometryBuffer.computeBoundingSphere()
+
+        Electron.instances.push(this)
+    }
+
+
+    static _reloadParticuleGeometryBuffer() {
+        // makes array of all the values of each position vector
+        let positionArray = Electron.instances.reduce((buffer, electron) => buffer.concat(electron.position.toArray()), [])
+        
+        // overwrite the geometry buffer with new values
+        let attributeBuffer = Electron._particuleGeometryBuffer.getAttribute("position")
+        attributeBuffer.set(positionArray, 0)
+        attributeBuffer.needsUpdate = true
+
+        // update buffer size
+        Electron._particuleGeometryBuffer.setDrawRange(0, Electron.instances.length)
+        Electron._particuleGeometryBuffer.computeBoundingSphere()
+        
+        // turn off the calling flag
+        Electron.bufferNeedsUpdate = false
+    }
+
     static init() {
         // init the instances object
         Electron.instances = []
@@ -13,27 +48,35 @@ class Electron {
         // create the buffer for the geometry
         const MAX_POINTS = 1000
 
-        // geometry
-        Electron.geometry = new THREE.BufferGeometry()
-        Electron.geometry.dynamic = true
-
-        // attributes
+        // attributes buffer
         let positions = new Float32Array(MAX_POINTS * 3)
         let buffer = new THREE.BufferAttribute(positions, 3)
-        Electron.geometry.addAttribute("position", buffer)
 
-        // load and setup the sprite
-        const electronImage = AssetManager.Get().textures.electron
+        // geometry buffer
+        Electron._particuleGeometryBuffer = new THREE.BufferGeometry()
+        Electron._particuleGeometryBuffer.dynamic = true
+        Electron._particuleGeometryBuffer.addAttribute("position", buffer)
 
-        const shapeMaterial = new THREE.PointsMaterial({
-            size: 0.5,
-            sizeAttenuation: true,
-            map: electronImage,
-            transparent: false,
-            alphaTest: 0.1
+        // create particle system for each overlay
+        Electron.overlays = {
+            shapeOverlay: new THREE.Points(Electron._particuleGeometryBuffer, Electron._getSolidMaterial()),
+            influenceOverlay: new THREE.Points(Electron._particuleGeometryBuffer, Electron._getInfluenceMaterial())
+        }
+        
+        // add particle systems to the scene
+        for (name in Electron.overlays) {
+            ThreeViewControllerInstance.addObjectToScene(Electron.overlays[name])
+        }
+
+        // add callback on render to check for updates
+        ThreeViewControllerInstance.callbackOnRender(() => {
+            if (Electron.bufferNeedsUpdate)
+                Electron._reloadParticuleGeometryBuffer()
         })
+    }
 
-        const influenceMaterial = new THREE.ShaderMaterial({
+    static _getInfluenceMaterial() {
+        return new THREE.ShaderMaterial({
             vertexShader: AssetManager.Get().shaders["influences.vs.glsl"],
             fragmentShader: AssetManager.Get().shaders["influences.fs.glsl"],
             uniforms: {
@@ -41,7 +84,7 @@ class Electron {
                     value: Electron.INFLUENCE_SIZE
                 },
                 color: {
-                    value: new THREE.Color(0xff0000)
+                    value: new THREE.Color(Electron.INFLUENCE_COLOR)
                 },
                 opacity: {
                     value: 0.5
@@ -54,53 +97,22 @@ class Electron {
             depthTest : true,
             depthFunc : THREE.NeverDepth
         })
-
-        Electron.overlays = {
-            shapeOverlay: new THREE.Points(Electron.geometry, shapeMaterial),
-            influenceOverlay: new THREE.Points(Electron.geometry, influenceMaterial)
-        }
-        
-        for (name in Electron.overlays) {
-            ThreeViewControllerInstance.addObjectToScene(Electron.overlays[name])
-        }
     }
 
-    static recreate() {
-        Electron.instances.forEach((electron, bufferindex) => {
-            const position = electron.position
-            positions[bufferindex * 3 + 0] = position.x
-            positions[bufferindex * 3 + 1] = position.y
-            positions[bufferindex * 3 + 2] = position.z
+
+    static _getSolidMaterial() {
+        return new THREE.PointsMaterial({
+            size: 0.5,
+            sizeAttenuation: true,
+            map: AssetManager.Get().textures.electron,
+            transparent: false,
+            alphaTest: 0.1
         })
-
-        Electron.geometry.setDrawRange(0, Electron.instances.length)
-        Electron.geometry.attributes.position.needsUpdate = true
-        Electron.geometry.computeBoundingSphere()
-
-        Electron.needsUpdate = false
     }
 
-    constructor(dot) {
-        this.dot = dot
-        this.charge = 1.0
-
-        const x = dot.position.x
-        const y = dot.position.y
-        const z = dot.position.z
-        var positions = Electron.geometry.attributes.position.array
-        let i = Electron.instances.length * 3
-        positions[i + 0] = x
-        positions[i + 1] = y
-        positions[i + 2] = z
-        Electron.geometry.setDrawRange(0, i + 1)
-        Electron.geometry.attributes.position.needsUpdate = true
-        Electron.geometry.computeBoundingSphere()
-
-        Electron.instances.push(this)
-        Electron.needsUpdate = true
-    }
 }
 
 Electron.RADIUS = 0.8
 Electron.INFLUENCE_SIZE = 2000.0
-Electron.needsUpdate = false
+Electron.INFLUENCE_COLOR = 0x00ff00
+Electron.bufferNeedsUpdate = false
