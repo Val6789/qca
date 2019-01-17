e2a82ee1f961c3691b076ec88b5a1533092a0cd9
max <mmmaxou@yahoo.fr>
2019-01-17 19:39:32 +0100

    Achievements css

--- src/scripts/GUI.js
+++ src/scripts/GUI.js
@@ -15,33 +15,53 @@ document.getElementById("toggle-activity-tab")
         return false
     })
 
+AchievementManager.OnReady(() => {
+    let populateAchievements = (function () {
+        const $achievements = document.getElementById("achievements-holder")
+        const $template = document.getElementById("template-achievement")
+        const achievements = AchievementManager.Get().achievements
+        for (let key in achievements) {
+            let achievement = achievements[key]
+            let node = $template.cloneNode(true)
 
-let populateAchievements = (function () {
-    const $achievements = document.getElementById("achievements-holder")
-    const $template = document.getElementById("template-achievement")
-    const achievements = AchievementManager.Get().achievements
-    for (let key in achievements) {
-        let achievement = achievements[key]
-        let node = $template.cloneNode(true)
+            // Image
+            if (achievement.image) {
+                node.children[0].src = "assets/textures/achievements/" + achievement.image + achievement.imageExtension
+            }
 
-        // Image
-        if (achievement.image) {
-            node.children[0].src = "assets/textures/achievements/" + achievement.image + achievement.imageExtension
+            // Title
+            node.children[1].innerText = achievement.fullname
+
+            // Description
+            node.children[2].innerText = achievement.description
+
+            // Remove id
+            node.removeAttribute("id")
+
+            // Append
+            $achievements.appendChild(node)
         }
+        $template.remove()
+        return true
+    })() || false
+    console.assert(populateAchievements, "Achievements were not populated")
 
-        // Title
-        node.children[1].innerText = achievement.fullname
+    let resize = (function () {
+        const $achievements = document.getElementById("achievements-holder")
 
-        // Description
-        node.children[2].innerText = achievement.description
-
-        // Remove id
-        node.removeAttribute("id")
-
-        // Append
-        $achievements.appendChild(node)
-    }
-    $template.remove()
-    return true
-})() || false
-console.assert(populateAchievements, "Achievements were not populated")
+        function maxHeight() {
+            $achievements.style.height = "auto"
+            var height = ($achievements.offsetHeight)
+            height += $achievements.childElementCount * 20
+            height = height / 3
+            height += 100
+            $achievements.style.height = height + "px"
+        }
+        window.onresize = () => {
+            maxHeight()
+        }
+        maxHeight()
+        return true
+    })() || false
+    console.assert(resize, "Resizing achievement in window failed")
+})
