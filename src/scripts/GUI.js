190da953f59437617cf8e7d87be451896ba9cefa
max <mmmaxou@yahoo.fr>
2019-01-17 19:04:03 +0100

    populate achievements in html

--- src/scripts/GUI.js
+++ src/scripts/GUI.js
@@ -14,3 +14,34 @@ document.getElementById("toggle-activity-tab")
         }
         return false
     })
+
+
+let populateAchievements = (function () {
+    const $achievements = document.getElementById("achievements-holder")
+    const $template = document.getElementById("template-achievement")
+    const achievements = AchievementManager.Get().achievements
+    for (let key in achievements) {
+        let achievement = achievements[key]
+        let node = $template.cloneNode(true)
+
+        // Image
+        if (achievement.image) {
+            node.children[0].src = "assets/textures/achievements/" + achievement.image + achievement.imageExtension
+        }
+
+        // Title
+        node.children[1].innerText = achievement.fullname
+
+        // Description
+        node.children[2].innerText = achievement.description
+
+        // Remove id
+        node.removeAttribute("id")
+
+        // Append
+        $achievements.appendChild(node)
+    }
+    $template.remove()
+    return true
+})() || false
+console.assert(populateAchievements, "Achievements were not populated")
