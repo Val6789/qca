eb1e79e22c6312aac5b0f80fffe167f6bc9a344f
bmantova <baptiste.mantovani@laposte.net>
2019-01-21 10:32:19 +0100

    Export on keypress E

--- src/scripts/KeyBoardController.js
+++ src/scripts/KeyBoardController.js
@@ -7,7 +7,7 @@ window.onkeyup = function(e){
     const KEY_E = 69;
     switch(e.keyCode){
         case KEY_E:
-            //Preset.exportQuantomAutomata(new AppController().automata);
+            Preset.exportQuantomAutomata(new AppController().automata);
             break;
     }
 }
