113fc0f9a094d07110259965df68fe1059e03d8b
Pierre Gabory <piergabory@gmail.com>
2019-01-28 19:29:13 +0100

    IO blocks are bigger and more opaque

--- src/scripts/model/OutputBlock.js
+++ src/scripts/model/OutputBlock.js
@@ -16,9 +16,16 @@ class OutputBlock extends Qubit {
         super(position, 0, false)
         this.type = "output"
         this.setLabel("?")
+        this.setSublabel("out")
 
         OutputBlock.instances.add(this)
+        this.object.scale.copy(InputBlock.BLOCK_SCALING)
+        this.family.material.opacity = 0.95
+
+        this.DEFAULT_COLOR = "#333"
+        this._showFamilyColor(Qubit._areFamilyColorsVisible)
     }
 }
 
 OutputBlock.instances = new Set()
+OutputBlock.BLOCK_SCALING = new THREE.Vector3(1.2,2,1.2)
